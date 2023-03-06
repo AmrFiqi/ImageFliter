9df8345ab84561fbf8d186349317127077e6a5f0
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-03-06 20:52:16 +0400

    Basic UI

--- ImageFliter/ViewController.swift
+++ ImageFliter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeFliter(_ sender: Any) {
+    }
+    
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
 }
 
