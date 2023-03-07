2811782a37516c9e11d35f1aae72a5eba0166837
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-03-07 23:49:51 +0400

    Set up methods

--- ImageFliter/ViewController.swift
+++ ImageFliter/ViewController.swift
@@ -7,25 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupNavigation()
     }
 
-
+    // MARK: - UI Items Method
+    
     @IBAction func changeFliter(_ sender: Any) {
     }
-    
     
     @IBAction func intensityChanged(_ sender: Any) {
     }
     
     @IBAction func save(_ sender: Any) {
+    }
+    
+    // MARK: - Class Methods
+    
+    func setupNavigation(){
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importImages))
+    }
+    
+    @objc func importImages(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // MARK: - ImagePickerController Delegate Methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return}
+        dismiss(animated: true)
+        currentImage = image
     }
 }
 
